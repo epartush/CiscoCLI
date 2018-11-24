834c4cc1e0c047e7a1db6c7b41ec6c9d7769dc98
EP <epartush@gmail.com>
2018-11-24 15:39:22 +0200

    my devices changed to new devices int hte new subnet 10.0.0.0 dev - edit changed to accept number 0    the problem was 10.0.0.150 -->  octet 2 and 3 had 0
    connect - recevie buffer changed from 512 to 1024 to accept banner

--- EP/scenario.py
+++ EP/scenario.py
@@ -16,7 +16,13 @@ def ipsec():
     side_a=devices[0]
     side_b=devices[1]
     #print '-'*20+'\n'+'| '+ ' '*1+'ipsec'
-    print "Side A"+' '*40+"Side B"
-    print "Mode: "+side_a['mode'] + ' '*15 + "Mode: "+side_b['mode']
-    print "WAN Interface["+side_a['int']+']' + ' '*15+'WAN Interface['+side_b['int']+']'
-    print "WAN IP["+side_a['ip']+']' + ' '*15+'WAN IP['+side_b['ip']+']'
+    spaces = 50
+    table=['Mode: ','WAN Interface: ','WAN IP: ']
+
+    print "Side A"+' '*int(spaces-len('Side A')) +"Side B"+'\n'+'------'+' '*int(spaces-len('Side A'))+'------'
+    print table[0]+side_a['mode'] + ' '*int(spaces-len(side_a['mode'])-len(table[0]))+table[0]+side_b['mode']
+    print table[1]+side_a['int'] + ' '*int(spaces-len(side_a['int'])-len(table[1]))+table[1]+side_b['int']
+    print table[2]+side_a['ip'] + ' '*int(spaces-len(table[2])-len(side_a['ip']))+table[2]+side_b['ip']
+
+    #for line in table:
+    #    print line+side_a[]
